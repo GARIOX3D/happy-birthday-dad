941df538b296d75b73d611c7fa8d55f5670390b8
Ezra Miller <emiller@lsoc.org>
2015-03-17 17:45:54 -0500

    ok

---
+++ js/models/rocket.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "rocket.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 118826,
+        "faces"         : 121350,
+        "normals"       : 0,
+        "uvs"           : 119533,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "buffers": "rocket.bin"
+
+}
